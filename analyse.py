39b3acdd8cd1c171c54ee6d8aa6779acc3e27a1d
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2023-04-25 12:05:16 +0100

    Implemented analysis.

--- analyse.py
+++ analyse.py
@@ -1,9 +1,84 @@
 from analyse_functions import *
 
-if len(sys.argv) > 2:
-    testdir = sys.argv[1]
-    outputdir = sys.argv[2]
-else:
-    console.print(f"Using ./data for testdir and ./output for output.", style="bold white")
-    testdir = "./data"
-    outputdir = "./output"
+if len(sys.argv) == 2:
+    campdir = sys.argv[1]
+elif len(sys.argv) == 1:
+    console.print(f"No <campaign_directory> passed. Refer to README.md to find out about usage.", style="bold red")
+    sys.exit()
+
+if not os.path.exists(campdir):
+    console.print(f"Path doesn't exist: {campdir}", style="bold red")
+    sys.exit()
+
+# ? Get the files in the campaign directory.
+camp_files = [os.path.join(campdir, _) for _ in os.listdir(campdir)]
+
+if len(camp_files) == 0:
+    console.print(f"No files found in {campdir}. ", style="bold red")
+    sys.exit()
+    
+if os.path.join(campdir, "progress.json") not in camp_files:
+    console.print(f"No progress.json file found in {campdir}.", style="bold red")
+    sys.exit()
+    
+# ? Get the .txt files.
+txt_files = [_ for _ in camp_files if '.txt' in _]
+
+if len(txt_files) == 0:
+    console.print(f"No output.txt files found in {campdir}.", style="bold red")
+    sys.exit()
+    
+# ? Get the test folders from the campaign directory.
+camp_dir_tests = [_ for _ in camp_files if os.path.isdir( _ )]
+
+# ? Get the test statuses from the progress.json.
+with open(os.path.join(campdir, 'progress.json')) as f:
+    progress_data = json.load(f)
+    
+test_statuses = progress_data
+
+# ? Cross reference the available files with the test status tests.
+test_statuses = [item for item in test_statuses if item['test'] in [os.path.basename(_) for _ in camp_dir_tests] ]
+
+all_statuses = []
+for status in [item['status'] for item in test_statuses]:
+    if "prolonged" in status:
+        all_statuses.append("🔴")
+    else:
+        all_statuses.append("🟢")
+
+all_statuses_output = ""
+for i, item in enumerate(all_statuses):
+    all_statuses_output += f"{item} "
+    if (i + 1) % 20 == 0:
+        all_statuses_output += "\n"
+
+assert( len(test_statuses) == len(camp_dir_tests) )
+
+total_tests_count = len(test_statuses)
+punctual_tests = [test for test in test_statuses if test['status'] == 'punctual']
+punctual_tests_count = len(punctual_tests)
+prolonged_tests = [test for test in test_statuses if test['status'] == 'prolonged']
+prolonged_tests_count = len(prolonged_tests)
+
+# ? Find how many tests have expected number of files (that have data themselves).
+tests_with_expected_files = []
+for test in punctual_tests:
+    testdir = os.path.join(campdir, test['test'])
+    expected_file_count = get_expected_file_count_from_testname(test['test'])
+    actual_file_count = get_actual_file_count(testdir)
+    
+    if expected_file_count == actual_file_count:
+        tests_with_expected_files.append(test)
+        
+table = Table(title=f"Analysis of {campdir}", show_lines=True)
+
+table.add_column("Stat")
+table.add_column("Value")
+
+table.add_row("Total Tests", f"{total_tests_count}")
+table.add_row("Punctual Tests", f"{punctual_tests_count}")
+table.add_row("Prolonged Tests", f"{prolonged_tests_count}")
+table.add_row("All Statuses (20 per row)", f"{all_statuses_output}")
+
+console.print(table, style="bold white")
--- analyse_functions.py
+++ analyse_functions.py
@@ -1,7 +1,8 @@
+import json
 import os
-import sys
 import re
 import shutil
+import sys
 
 from pprint import pprint
 from rich.console import Console
@@ -11,3 +12,18 @@ from rich.table import Table
 install()
 console = Console(record=True)
 
+def get_expected_file_count_from_testname(test):
+    num_str = test.split("S")[0].split('_')[-1]
+    num_str = "".join(filter(str.isdigit, num_str))
+    num = int(num_str)
+    num += 1
+    
+    return num
+
+assert(get_expected_file_count_from_testname('600s_100B_1P_13S_rel_mc_0dur_100lc') == 14)
+
+def get_actual_file_count(testdir):
+    test_files = os.listdir(testdir)
+    csv_files_with_data = [os.path.join(testdir, file) for file in test_files if '.csv' in file and os.path.getsize( os.path.join(testdir, file) ) > 0]
+    
+    return len(csv_files_with_data)
